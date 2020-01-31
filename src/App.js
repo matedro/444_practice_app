0ea035100c338b8f3b570d6f6143c8561b79ce47
matedro <matedro1@gmail.com>
2020-01-30 22:44:45 -0500

    First draft of homepage. Still uses create-react-app's CSS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+import boring from './boring.jpg';
 import './App.css';
 
 function App() {
+  let imgStyle = {
+    maxWidth: "40%"
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          International Pencil Pushers Conference<br/>
+          Boring, Oregon, USA<br/>
+          May 6, 2020
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={boring} style={imgStyle} className="Boring" alt="Boring, Oregon" />
+        <p>
+          Please register with the button below.
+        </p>
+        <button>
+          Register
+        </button>
       </header>
     </div>
   );
