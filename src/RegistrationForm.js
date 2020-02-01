559fcea0bd4135034648d646e2df30c0c02e0ac0
matedro <matedro1@gmail.com>
2020-02-01 10:57:31 -0500

    RegistrationForm was double-exported

--- src/RegistrationForm.js
+++ src/RegistrationForm.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export class RegistrationForm extends React.Component {
+class RegistrationForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
